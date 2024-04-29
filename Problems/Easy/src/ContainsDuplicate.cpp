2a1ff1d0520abaf9695a8c57ac16b1da3456a5e8
DavidYeLuo <davidayeluo@gmail.com>
2024-04-28 21:33:44 -0700

    Modify to output wrong result.

--- Problems/Easy/src/ContainsDuplicate.cpp
+++ Problems/Easy/src/ContainsDuplicate.cpp
@@ -1,13 +1,5 @@
 #include "ContainsDuplicate.h"
 
 bool Easy::ContainsDuplicate::containsDuplicate(const std::vector<int> &nums) {
-  std::unordered_map<int, bool> map;
-  for (int i = 0; i < nums.size(); i++) {
-    if (map.find(nums[i]) == map.end()) {
-      map[nums[i]] = true;
-    } else {
-      return true;
-    }
-  }
-  return false;
+  return true
 }
