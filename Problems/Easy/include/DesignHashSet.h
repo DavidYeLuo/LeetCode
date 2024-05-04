b90dd73f5fe774ada439f1f3e2618470929ffd33
DavidYeLuo <davidayeluo@gmail.com>
2024-05-03 19:02:00 -0700

    Add implementation for the problem Design Hashset.

--- Problems/Easy/include/DesignHashSet.h
+++ Problems/Easy/include/DesignHashSet.h
@@ -1,10 +1,12 @@
 #pragma once
+#include <vector>
 namespace Easy {
 class DesignHashSet {
 public:
+  std::vector<int> v;
+  DesignHashSet() {}
   void add(int key);
   bool contains(int key);
   void remove(int key);
 };
-
 } // namespace Easy
--- Problems/Easy/src/DesignHashSet.cpp
+++ Problems/Easy/src/DesignHashSet.cpp
@@ -1,4 +1,21 @@
 #include "DesignHashSet.h"
-void Easy::DesignHashSet::add(int key) {}
-bool Easy::DesignHashSet::contains(int key) { return 0; }
-void Easy::DesignHashSet::remove(int key) {}
+void Easy::DesignHashSet::add(int key) {
+  if (contains(key))
+    return;
+  v.push_back(key);
+}
+bool Easy::DesignHashSet::contains(int key) {
+  for (int i = 0; i < v.size(); i++) {
+    if (v[i] == key)
+      return true;
+  };
+  return false;
+}
+void Easy::DesignHashSet::remove(int key) {
+  for (int i = 0; i < v.size(); i++) {
+    if (v[i] == key) {
+      v.erase(v.begin() + i);
+      break;
+    }
+  }
+}
