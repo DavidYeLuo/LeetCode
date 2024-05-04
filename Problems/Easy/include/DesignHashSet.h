1b00e0fad18655064de7809d50c7d6984a1f9ae2
DavidYeLuo <davidayeluo@gmail.com>
2024-05-03 17:55:45 -0700

    Add template 705. Design HashSet.

---
+++ Problems/Easy/include/DesignHashSet.h
@@ -0,0 +1,10 @@
+#pragma once
+namespace Easy {
+class DesignHashSet {
+public:
+  void add(int key);
+  bool contains(int key);
+  void remove(int key);
+};
+
+} // namespace Easy
---
+++ Problems/Easy/src/DesignHashSet.cpp
@@ -0,0 +1,4 @@
+#include "DesignHashSet.h"
+void Easy::DesignHashSet::add(int key) {}
+bool Easy::DesignHashSet::contains(int key) { return 0; }
+void Easy::DesignHashSet::remove(int key) {}
