7ebe300d33fa26261e289c4293b39cfb1311bce4
DavidYeLuo <davidayeluo@gmail.com>
2024-05-05 11:15:24 -0700

    Solve 706. Design HashMap.

--- Problems/Easy/include/DesignHashMap.h
+++ Problems/Easy/include/DesignHashMap.h
@@ -1,7 +1,18 @@
 #pragma once
+#include <vector>
 #define HASH_SET_SIZE 10000
 namespace Easy {
+class Node {
+public:
+  int key;
+  int val;
+  Node *next;
+  Node(int key, int val);
+};
 class DesignHashMap {
+private:
+  std::vector<Node *> v;
+
 public:
   DesignHashMap();
 
