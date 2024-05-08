a0e58aefc2a78f90d618be193cfe779a691e7423
DavidYeLuo <davidayeluo@gmail.com>
2024-05-08 06:21:59 -0700

    Revert "Improve SumRangeQueryImmutable using prefix sum."
    This reverts commit 7cfd5a6f518ff9c85313926bc49a7c849a244c34.

--- Problems/Easy/src/SumRangeQueryImmutable.cpp
+++ Problems/Easy/src/SumRangeQueryImmutable.cpp
@@ -1,14 +1,12 @@
 #include "SumRangeQueryImmutable.h"
 
 Easy::SumRangeQueryImmutable::SumRangeQueryImmutable(std::vector<int> &nums) {
-  prefix_sum.resize(nums.size());
-  prefix_sum[0] = nums[0];
-  for (int i = 1; i < nums.size(); i++) {
-    prefix_sum[i] = prefix_sum[i - 1] + nums[i];
-  }
+  this->nums = nums;
 };
 int Easy::SumRangeQueryImmutable::sumRange(int left, int right) {
-  if (left == 0)
-    return prefix_sum[right];
-  return prefix_sum[right] - prefix_sum[left - 1];
+  int sum = 0;
+  for (int i = left; i <= right; i++) {
+    sum += nums[i];
+  }
+  return sum;
 };
