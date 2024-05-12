51d00fb8414badda1aaf1ce172102088cba2f6c1
DavidYeLuo <davidayeluo@gmail.com>
2024-05-12 12:20:56 -0700

    Solve 724. Find Pivot Index using prefix sum strategy.

--- Problems/Easy/src/FindPivotIndex.cpp
+++ Problems/Easy/src/FindPivotIndex.cpp
@@ -1,3 +1,19 @@
 #include "FindPivotIndex.h"
 
-int Easy::FindPivot::findPivotIndex(std::vector<int> &nums) { return 0; }
+int Easy::FindPivot::findPivotIndex(std::vector<int> &nums) {
+  // Starting from postfix because starting from prefix doesn't pass test
+  std::vector<int> suffixSum;
+  suffixSum.resize(nums.size() + 1, 0);
+  for (int i = nums.size() - 1; i >= 0; i--) {
+    suffixSum[i] = nums[i] + suffixSum[i + 1];
+  }
+
+  int prefixSum = 0;
+  for (int i = 0; i < nums.size(); i++) {
+    if (suffixSum[i + 1] == prefixSum) {
+      return i;
+    }
+    prefixSum += nums[i];
+  }
+  return -1;
+}
