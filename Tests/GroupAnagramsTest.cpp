060d2f868e258934296b5251185969b2d2a889f0
DavidYeLuo <davidayeluo@gmail.com>
2024-05-21 19:45:49 -0700

    Add double hashmap approach(not very good but works) group anagram. Modify tests so that order in a list doesn't matter.

--- Tests/GroupAnagramsTest.cpp
+++ Tests/GroupAnagramsTest.cpp
@@ -1,19 +1,40 @@
 #include "GroupAnagrams.h"
 #include <gtest/gtest.h>
 namespace {
+// Order doesn't matter so sorting will just help us test it
 TEST(GroupAnagrams, SimpleTests) {
   Medium::GroupAnagrams instance;
+  std::vector<std::vector<std::string>> result;
   std::vector<std::string> input = {"eat", "tea", "tan", "ate", "nat", "bat"};
   std::vector<std::vector<std::string>> expected = {
       {"bat"}, {"nat", "tan"}, {"ate", "eat", "tea"}};
 
-  ASSERT_EQ(instance.groupAnagrams(input), expected);
+  // Order doesn't matter so sorting will just help us test it
+  auto ret = instance.groupAnagrams(input);
+  std::sort(expected.begin(), expected.end());
+  std::sort(ret.begin(), ret.end());
+  for (auto &element : ret) {
+    std::sort(element.begin(), element.end());
+  }
+  ASSERT_EQ(ret, expected);
+
   input = {""};
   expected = {{""}};
-  ASSERT_EQ(instance.groupAnagrams(input), expected);
+  ret = instance.groupAnagrams(input);
+  std::sort(expected.begin(), expected.end());
+  std::sort(ret.begin(), ret.end());
+  for (auto &element : ret) {
+    std::sort(element.begin(), element.end());
+  }
+
   input = {"a"};
   expected = {{"a"}};
-  ASSERT_EQ(instance.groupAnagrams(input), expected);
+  ret = instance.groupAnagrams(input);
+  std::sort(expected.begin(), expected.end());
+  std::sort(ret.begin(), ret.end());
+  for (auto &element : ret) {
+    std::sort(element.begin(), element.end());
+  }
 
 } // Test
 } // namespace
