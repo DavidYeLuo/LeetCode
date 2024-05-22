60e0b08bf4299834aa0bb946ec861b25c10d8ec5
DavidYeLuo <davidayeluo@gmail.com>
2024-05-21 19:45:49 -0700

    Add double hashmap approach(not very good but works) group anagram. Modify tests so that order in a list doesn't matter.

--- Problems/Medium/src/GroupAnagrams.cpp
+++ Problems/Medium/src/GroupAnagrams.cpp
@@ -1,8 +1,35 @@
 #include "GroupAnagrams.h"
+#include <algorithm>
+#include <unordered_map>
 
 Medium::GroupAnagrams::GroupAnagrams() {}
 
 std::vector<std::vector<std::string>>
 Medium::GroupAnagrams::groupAnagrams(std::vector<std::string> &strs) {
-  return {{}};
+  std::vector<std::vector<std::string>> result;
+
+  std::unordered_map<std::string, std::unordered_map<std::string, int>> map;
+
+  for (int i = 0; i < strs.size(); i++) {
+    std::string sorted_string = strs[i];
+    std::sort(sorted_string.begin(), sorted_string.end());
+    if (map.find(sorted_string) == map.end()) {
+      map[sorted_string] = std::unordered_map<std::string, int>();
+      map[sorted_string][strs[i]] = 1;
+    } else {
+      map[sorted_string][strs[i]]++;
+    }
+  }
+
+  for (const auto &[key, value] : map) {
+    std::vector<std::string> temp;
+    for (const auto &[innerKey, innerValue] : value) {
+      for (int i = 0; i < innerValue; i++) {
+        temp.push_back(innerKey);
+      }
+    }
+    result.push_back(temp);
+  }
+
+  return result;
 }
