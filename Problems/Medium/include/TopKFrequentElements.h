faae9da7f24163e72983a5fd2665d81e2a7e41fc
DavidYeLuo <davidayeluo@gmail.com>
2024-05-24 23:57:47 -0700

    Add working solution to 347. Top K Frequent ELements.

--- Problems/Medium/include/TopKFrequentElements.h
+++ Problems/Medium/include/TopKFrequentElements.h
@@ -1,10 +1,23 @@
 #pragma once
 
+#include <unordered_map>
 #include <vector>
 namespace Medium {
+class Node {
+public:
+  int val;
+  int freq;
+  Node *left;
+  Node *right;
+  Node(int val, int freq) : val(val), freq(freq) {}
+};
 class TopKFrequentElements {
 public:
   TopKFrequentElements();
   std::vector<int> topKFrequentElements(std::vector<int> &nums, int k);
+
+private:
+  void Remove(Node *node, std::unordered_map<int, Node *> &map);
+  void Add(Node *node, Node *rightNode);
 };
 } // namespace Medium
--- Problems/Medium/src/TopKFrequentElements.cpp
+++ Problems/Medium/src/TopKFrequentElements.cpp
@@ -1,9 +1,36 @@
 #include "TopKFrequentElements.h"
+#include <string>
+
+using std::string;
 
 Medium::TopKFrequentElements::TopKFrequentElements() {}
 
 std::vector<int>
 Medium::TopKFrequentElements::topKFrequentElements(std::vector<int> &nums,
                                                    int k) {
-  return {};
+  std::vector<int> result;
+  std::unordered_map<int, int> numFreq(0);
+  std::vector<std::vector<int>> freqNum(nums.size() + 1);
+  for (int i = 0; i < nums.size(); i++) {
+    if (numFreq.find(nums[i]) == numFreq.end()) {
+      numFreq[nums[i]] = 1;
+    } else {
+      numFreq[nums[i]]++;
+    }
+  }
+  for (const auto &[num, freq] : numFreq) {
+    freqNum[freq].push_back(num);
+  }
+  int count = 0;
+  for (int i = freqNum.size() - 1; i >= 0; i--) {
+    if (count == k)
+      return result;
+    if (freqNum[i].empty())
+      continue;
+    for (const auto &element : freqNum[i]) {
+      result.push_back(element);
+      count++;
+    }
+  }
+  return result;
 }
