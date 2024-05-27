5d0526c32cf62ef3a49d2d44151308cc1b8d3390
DavidYeLuo <davidayeluo@gmail.com>
2024-05-27 11:20:55 -0700

    Add template 36. Valid Sudoku.

---
+++ Problems/Medium/include/ValidSudoku.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+namespace Medium {
+class ValidSudoku {
+public:
+  ValidSudoku();
+  bool isValidSudoku(std::vector<std::vector<char>> board);
+};
+} // namespace Medium
---
+++ Problems/Medium/src/ValidSudoku.cpp
@@ -0,0 +1,7 @@
+#include "ValidSudoku.h"
+
+Medium::ValidSudoku::ValidSudoku() {}
+
+bool Medium::ValidSudoku::isValidSudoku(std::vector<std::vector<char>> board) {
+  return false;
+}
