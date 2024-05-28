5918c8718257ed60f385d5fc7b152c9a9781b296
DavidYeLuo <davidayeluo@gmail.com>
2024-05-27 19:30:57 -0700

    Solve 36. Valid Sudoku using hashmaps.

--- Problems/Medium/src/ValidSudoku.cpp
+++ Problems/Medium/src/ValidSudoku.cpp
@@ -1,7 +1,27 @@
 #include "ValidSudoku.h"
+#include <cstdio>
+#include <set>
 
 Medium::ValidSudoku::ValidSudoku() {}
 
 bool Medium::ValidSudoku::isValidSudoku(std::vector<std::vector<char>> board) {
-  return false;
+  const int SIZE = 9;
+  std::set<char> rowSet[SIZE];
+  std::set<char> colSet[SIZE];
+  std::set<char> boxSet[3][3];
+
+  for (int row = 0; row < SIZE; row++) {
+    for (int col = 0; col < SIZE; col++) {
+      char c = board[row][col];
+      if (c == '.')
+        continue;
+      if (rowSet[row].contains(c) || colSet[col].contains(c) ||
+          boxSet[row / 3][col / 3].contains(c))
+        return false;
+      rowSet[row].insert(c);
+      colSet[col].insert(c);
+      boxSet[row / 3][col / 3].insert(c);
+    }
+  }
+  return true;
 }
