53aac8bdd06a59f4ad2cd8843f5fd29fba62d9b6
DavidYeLuo <davidayeluo@gmail.com>
2024-05-29 05:58:28 -0700

    Add tests 128. Longest Consecutive Sequence.

--- Tests/LongestConsecutiveSequenceTest.cpp
+++ Tests/LongestConsecutiveSequenceTest.cpp
@@ -2,8 +2,16 @@
 #include <gtest/gtest.h>
 namespace {
 TEST(LongestConsecutiveSequence, SimpleTests) {
-  bool expected = true;
-  bool result = false;
-  ASSERT_EQ(expected, result);
+  std::vector<int> nums;
+  int expected;
+  Medium::LongestConsecutiveSequence instance;
+
+  nums = {100, 4, 200, 1, 3, 2};
+  expected = 4;
+  ASSERT_EQ(expected, instance.longestConsecutiveSequence(nums));
+
+  nums = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+  expected = 9;
+  ASSERT_EQ(expected, instance.longestConsecutiveSequence(nums));
 } // Test
 } // namespace
