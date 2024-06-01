b0fe531d6eaa8bae7c1d320b5c609b28cbdf8ae4
DavidYeLuo <davidayeluo@gmail.com>
2024-06-01 11:51:05 -0700

    Add solution 682. Baseball Game.

--- Problems/Easy/src/BaseballGame.cpp
+++ Problems/Easy/src/BaseballGame.cpp
@@ -1,7 +1,31 @@
 #include "BaseballGame.h"
+#include <cctype>
+#include <stack>
 
 Easy::BaseballGame::BaseballGame() {}
 
 int Easy::BaseballGame::calPoints(std::vector<std::string> &operations) {
-  return 0;
+  std::stack<int> s;
+  for (int i = 0; i < operations.size(); i++) {
+    if (operations[i] == "+") {
+      int b = s.top();
+      s.pop();
+      int a = s.top();
+      s.push(b);
+      s.push(a + b);
+    } else if (operations[i] == "D") {
+      s.push(s.top() * 2);
+    } else if (operations[i] == "C") {
+      s.pop();
+    } else {
+      s.push(stoi(operations[i]));
+    }
+  }
+
+  int sum = 0;
+  while (!s.empty()) {
+    sum += s.top();
+    s.pop();
+  }
+  return sum;
 }
