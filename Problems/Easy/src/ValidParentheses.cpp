e6cdce97537df5aaec774af46dc900e6eb72383f
DavidYeLuo <davidayeluo@gmail.com>
2024-06-02 11:30:37 -0700

    Add solution 20. Valid Parentheses.

--- Problems/Easy/src/ValidParentheses.cpp
+++ Problems/Easy/src/ValidParentheses.cpp
@@ -1,5 +1,23 @@
 #include "ValidParentheses.h"
+#include <stack>
+#include <unordered_map>
 
 Easy::ValidParentheses::ValidParentheses() {}
 
-bool Easy::ValidParentheses::isValid(std::string s) { return false; }
+bool Easy::ValidParentheses::isValid(std::string s) {
+  std::stack<char> st;
+
+  for (int i = 0; i < s.length(); i++) {
+    if (st.empty()) {
+      st.push(s[i]);
+    } else if (s[i] == ')' && st.top() == '(' ||
+               s[i] == ']' && st.top() == '[' ||
+               s[i] == '}' && st.top() == '{') {
+      st.pop();
+    } else {
+      st.push(s[i]);
+    }
+  }
+
+  return st.empty();
+}
